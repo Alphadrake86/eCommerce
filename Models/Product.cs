d1a0627e79c8e8e87a89056f3e879ed7bb66e75c
Alphadrake86 <anderson8221@student.cptc.edu>
2020-07-29 12:28:29 -0700

    Added documentation to product model

--- Models/Product.cs
+++ Models/Product.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace eCommerce.Models
 {
+    /// <summary>
+    /// A sellable product
+    /// </summary>
     public class Product
     {
         [Key]
         public int ProductId { get; set; }
 
+        /// <summary>
+        /// The title of the product
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// The price of the currency, in US dollars
+        /// </summary>
         public double Price { get; set; }
 
+        /// <summary>
+        /// the user generated category this product falls under (Eg. Appliances, Electronics, Kitchen, etc.)
+        /// </summary>
         public string Category { get; set; }
     }
 }
