e41f8a8bbf91eb140f524007eee09b8e69d91739
Erik <57242694+Alphadrake86@users.noreply.github.com>
2020-07-30 15:06:39 -0700

    Issue#4 create products db (#7)
    * Added product model
    
    * Added documentation to product model
    
    * Added Database thigidoos
    
    * Added Dbcontext to startup
    
    * Don't worry about startup

---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// A sellable product
+    /// </summary>
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The title of the product
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The price of the currency, in US dollars
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// the user generated category this product falls under (Eg. Appliances, Electronics, Kitchen, etc.)
+        /// </summary>
+        public string Category { get; set; }
+    }
+}
