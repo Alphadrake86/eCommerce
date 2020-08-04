dd919e65ecaa8cc2df5406b7f5db133273afc212
Alphadrake86 <anderson8221@student.cptc.edu>
2020-08-04 09:17:29 -0700

    added database code to product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerce.Data;
+using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.Controllers
 {
     public class ProductController : Controller
     {
+        readonly ProductContext _context;
+
+        public ProductController(ProductContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Product> products = _context.Products.ToList();
+
+
+            return View(products);
         }
     }
 }
