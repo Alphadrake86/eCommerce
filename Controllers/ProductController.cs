a347dad413a713849de462ee6ba382c87d3f18d6
Erik <57242694+Alphadrake86@users.noreply.github.com>
2020-08-11 11:41:56 -0700

    Issue#20 add delete (#23)
    * delete confirmation page added
    
    * Add delete post method

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -81,5 +81,31 @@ namespace eCommerce.Controllers
             return View(p);
         }
 
+        public async Task<IActionResult> Delete(int id)
+        {
+            Product p = await _context.Products
+                .Where(p => p.ProductId == id)
+                .SingleAsync();
+
+            return View(p);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmAsync(int id)
+        {
+            Product p = await _context.Products
+                .Where(prod => prod.ProductId == id)
+                .SingleAsync();
+
+            _context.Entry(p).State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
+
+            TempData["Message"] = "Product removed successfully";
+
+            // redirect to catalog page
+            return RedirectToAction("Index");
+        }
     }
 }
